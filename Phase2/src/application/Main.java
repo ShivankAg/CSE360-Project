bc00eb7ce364445121084c5dd171d22b1c62400a
Karthik350 <51489980+Karthik350@users.noreply.github.com>
2022-11-11 20:01:01 -0700

    Add files via upload
    This is the Chef Screen. It updates the data in the .txt file

---
+++ Phase2/src/application/Main.java
@@ -0,0 +1,35 @@
+package application;
+	
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+
+    private static Stage stg;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        stg = primaryStage;
+        primaryStage.setResizable(false);
+        Parent root = FXMLLoader.load(getClass().getResource("AcceptNewOrder.fxml"));
+        primaryStage.setTitle("Chef Screen");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+
+    public void changeScene(String fxml) throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+        stg.getScene().setRoot(pane);
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
